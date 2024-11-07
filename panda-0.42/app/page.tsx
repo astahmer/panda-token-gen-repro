0b80da3797e69cf68f10fd6a6cf3961d7743c297
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-11-07 21:46:31 +0100

    chore: minimal repro + fix

--- panda-0.42/app/page.tsx
+++ panda-0.42/app/page.tsx
@@ -9,12 +9,14 @@ import {
 	Dialog,
 	Select,
 	Square,
-} from "@omnidev/sigil";
+// } from "@omnidev/sigil";
+// } from "../node_modules/@omnidev/sigil/build/sigil.js"
+} from "#omnidev/sigil";
 
 const HomePage = () => (
 	<Container p={6}>
 		Square with red.500 background color:
-		<Square size={20} bgColor="red.500" />
+		<Square size={20} bgColor="red.500" padding="50px" border="1px solid blue" />
 	</Container>
 );
 
--- panda-0.42/panda.config.ts
+++ panda-0.42/panda.config.ts
@@ -5,8 +5,9 @@ const pandaConfig = defineConfig({
 	preflight: true,
 	jsxFramework: "react",
 	presets: ["@pandacss/preset-base", sigilPreset],
-	include: ["src/**/*.{ts,tsx}"],
+	include: ["{app, src}/**/*.{ts,tsx}"],
 	outdir: "src/generated/panda",
+	importMap: "#omnidev/sigil",
 	staticCss: {
 		css: [
 			{
